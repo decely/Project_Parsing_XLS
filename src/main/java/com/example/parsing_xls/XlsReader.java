f9eb87b9d358fa11019c51fb7fefb50adfd32804
decely <danil.ratushnyj@mail.ru>
2023-04-27 21:01:46 +0300

    XLSRead

--- src/main/java/com/example/parsing_xls/XlsReader.java
+++ src/main/java/com/example/parsing_xls/XlsReader.java
@@ -1,34 +1,51 @@
 package com.example.parsing_xls;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 
 
 public class XlsReader {
     public static void main() throws IOException {
-        FileInputStream inputStream = new FileInputStream("D:\\Test\\Test.xls");
 
+        //Чтение файла с помощью Workbook
+        FileInputStream inputStream = new FileInputStream("D:\\Test\\Test2.xls");
         Workbook workbook = null;
         workbook = WorkbookFactory.create(inputStream);
-        Sheet firstSheet = workbook.getSheetAt(0);
-        Iterator<Row> iterator = firstSheet.iterator();
-        while (iterator.hasNext()) {
-            Row nextRow = iterator.next();
-            Iterator<Cell> cellIterator = nextRow.cellIterator();
+        //Workbook workbook = new XSSFWorkbook(inputStream);
+        //Объявление итератора листов
+        Iterator<Sheet> sheetIterator = workbook.sheetIterator();
 
-            while (cellIterator.hasNext()) {
-                Cell cell = cellIterator.next();
-                        System.out.print(cell.getStringCellValue());
-                        break;
+        //Начальное положение итератора листов
+        Sheet sheet = workbook.getSheetAt(0);
+
+        DataFormatter dataFormatter = new DataFormatter();
+
+        System.out.println("\n\nЧтение файла XLS через итератор\n");
+
+        //Цикл чтения листов
+        while (sheetIterator.hasNext()) {
+            sheet = sheetIterator.next();
+            Iterator<Row> rowIterator = sheet.rowIterator();
+            System.out.println("=> " + sheet.getSheetName());
+
+            //Цикл чтения строк
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+            //Цикл чтения ячеек
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    String cellValue = dataFormatter.formatCellValue(cell);
+
+                    //Вывод прочитанной ячейки в консоль
+                    System.out.print(cellValue + "\t");
                 }
-                System.out.print(" - ");
+                System.out.println();
             }
-            System.out.println();
         }
+    }
 }
